bac4944d9521c3d7a5d99eb3e28f289e32d007bd
xshche05 <xshche05@stud.fit.vutbr.cz>
2023-12-06 16:36:46 +0100

    Fixes

---
+++ odevzdani.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+mkdir "odevzdani"
+cp "src/main.c" "odevzdani"
+cp "src/macros.h" "odevzdani"
+
+
+cp "src/utils/list.h" "odevzdani"
+cp "src/utils/list.c" "odevzdani"
+
+cp "src/utils/memory.h" "odevzdani"
+cp "src/utils/memory.c" "odevzdani"
+
+cp "src/utils/source_file.h" "odevzdani"
+cp "src/utils/source_file.c" "odevzdani"
+
+cp "src/utils/stack.h" "odevzdani"
+cp "src/utils/stack.c" "odevzdani"
+
+cp "src/utils/string_util.h" "odevzdani"
+cp "src/utils/string_util.c" "odevzdani"
+
+cp "src/parts/codegen/codegen.h" "odevzdani"
+cp "src/parts/codegen/codegen.c" "odevzdani"
+
+cp "src/parts/parser/expr_parser.h" "odevzdani"
+cp "src/parts/parser/expr_parser.c" "odevzdani"
+
+cp "src/parts/parser/parser.h" "odevzdani"
+cp "src/parts/parser/parser.c" "odevzdani"
+
+cp "src/parts/parser/symtable.h" "odevzdani"
+cp "src/parts/parser/symtable.c" "odevzdani"
+
+cp "src/parts/scanner/scanner.c" "odevzdani"
+cp "src/parts/scanner/scanner.h" "odevzdani"
+
+cp "src/parts/scanner/token.h" "odevzdani"
+cp "src/parts/scanner/token.c" "odevzdani"
+
+
+cp "src/Makefile" "odevzdani"
+cp "src/rozdeleni" "odevzdani"
+cp "src/rozsireni" "odevzdani"
+cp "src/dokumentace.pdf" "odevzdani"
